731b0973b54482f15912b410c868d6c812675e45
unknown <mdanko990@gmail.com>
2026-02-13 22:21:11 +0100

    make input collaborative

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,40 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { Input } from "@heroui/input";
+import { Avatar, AvatarGroup, Chip, Tooltip } from "@heroui/react";
+import { FocusEventHandler, useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-
 export default function Home() {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
   const socketRef = useRef<Socket | null>(null);
-  
+  const [editor, setEditor] = useState("");
+  const [users, setUsers] = useState<string[]>([]);
+  const [me, setMe] = useState<string>("");
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     const init = async () => {
       const socket = io("http://localhost:3000");
       socketRef.current = socket;
 
       socket.on("connect", () => {
-        console.log("Connected:", socket.id);
+        socketRef.current?.emit("add-user", socket.id);
+        setMe(socket.id ?? "");
       });
 
       socket.on("update-input", (msg: string) => {
         setInput(msg);
+      });
+
+      socket.on("update-editor", (id) => {
+        setEditor(id);
+        if (id) setIsOpen(true);
+        if (!id) setIsOpen(false);
+      });
+
+      socket.on("users-list", (list: string[]) => {
+        setUsers(list);
       });
     };
 
@@ -28,22 +44,50 @@ export default function Home() {
       socketRef.current?.disconnect();
     };
   }, []);
-  
+
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInput(value);
-
-    console.log("Emitting:", value);
     socketRef.current?.emit("input-change", value);
   };
 
-  return <div>
-      <input
-      placeholder="type text"
-      value={input}
-      // onInput={onChangeHandler}
-      onChange={onChangeHandler}
+  const onFocusHandler = (e: any) => {
+    setEditor(socketRef.current?.id!);
+    socketRef.current?.emit("input-focus", socketRef.current?.id!);
+  };
+
+  const onBlurHandler = (e: any) => {
+    setEditor("");
+    socketRef.current?.emit("input-focus", "");
+  };
+
+  return (
+    <div className="flex flex-col gap-3 p-10">
+      <AvatarGroup isBordered>
+        {users.map((u) => (
+          <Avatar
+            name={u}
+            color={
+              me === u ? "primary" : editor === u ? "secondary" : "default"
+            }
+          />
+        ))}
+      </AvatarGroup>
+      {editor != me && editor && (
+        <div className="flex w-full justify-end">
+          <Chip size="sm" color="secondary">
+            {editor}
+          </Chip>
+        </div>
+      )}
+      <Input
+        placeholder="type text"
+        value={input}
+        onFocus={onFocusHandler}
+        onBlur={onBlurHandler}
+        onChange={onChangeHandler}
+        variant="bordered"
       />
-      <div>{input}</div>
     </div>
+  );
 }
--- server.js
+++ server.js
@@ -9,14 +9,33 @@ const handler = app.getRequestHandler();
 app.prepare().then(() => {
   const httpServer = createServer(handler);
   const io = new Server(httpServer);
+  const connectedUsers = new Set();
 
   io.on("connection", (socket) => {
     console.log("Connected:", socket.id);
+    connectedUsers.add(socket.id);
+    io.emit("users-list", Array.from(connectedUsers));
 
-    socket.on('input-change', msg => {
-        console.log('input-change')
-        socket.broadcast.emit('update-input', msg)
-    })
+    socket.on("input-change", (msg) => {
+      socket.broadcast.emit("update-input", msg);
+    });
+
+    socket.on("input-focus", (id) => {
+      console.log("input-focus");
+      socket.broadcast.emit("update-editor", id);
+    });
+
+    socket.on("input-change", (msg) => {
+      socket.broadcast.emit("update-input", msg);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected:", socket.id);
+
+      connectedUsers.delete(socket.id);
+
+      io.emit("users-list", Array.from(connectedUsers));
+    });
   });
 
   httpServer.listen(3000, () => {
